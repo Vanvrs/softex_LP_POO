3dc78349599aa9238de3fba2a3ff8c024b21f72e
Vanvrs <107437663+Vanvrs@users.noreply.github.com>
2022-07-19 12:18:39 -0300

    Add files via upload

---
+++ ConfigurandoAmbienteJS.java
@@ -0,0 +1,32 @@
+package AprendendoJavaScript;
+
+public class ConfigurandoAmbienteJS {
+
+//Atividade proposta: Com ajuda da leitura complementar, 
+//você deve configurar o seu ambiente de desenvolvimento
+//JavaScript. Esse ambiente pode ser uma IDE ou um editor de 
+//código-fonte. Para isso, você deve levar em consideração alguns
+//pontos: 
+
+//O seu ambiente precisa programar na linguagem JavaScript; 
+//Se necessário, configure seu ambiente; 
+//Deixe ele pronto para uso, ou seja, sem mais configurações. 
+//Escreva, no mínimo, um parágrafo mostrando seu ambiente configurado,
+//explique sua escolha. 
+	
+	
+//RESPOSTA:
+	//O ambiente escolhido e configurado para programar em JavaScript 
+	//foi o editor de código VScode.
+	//Foi instalado e logo em seguida no botão de extensão foi baixado 
+	//o NodejS para auxiliar durante a programação, visto que 
+	//possibilita programar sem precisar sair da VScode para
+	//executar o código no Brower acionando apenas o crtl+Alt+N 
+	//e o arquivo é executado e é possível ver o output uma janela ao 
+	//lado, na mesma área onde o código foi programado.
+	//A escolha do VScode foi devido aos comentários positivos sobre 
+	//o mesmo e a versatilidade que possibilita instalar extensões de acordo
+	//com a necessidade do programador.
+	
+	//
+}
